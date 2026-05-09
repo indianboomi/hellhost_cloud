869d48f860f1363afed5bb5dcc566479afb2eb72
indianboomi <indianboomi157@gmail.com>
2026-05-09 21:24:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
-function showMessage() {
-    alert("Hello! Your site is working 🎉");
-}
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+// Your config
+const firebaseConfig = {
+  apiKey: "AIzaSyArPkzW5MtYGqsfBPyargJk0QXt_gAoW4A",
+  authDomain: "hellhost-d12d4.firebaseapp.com",
+  projectId: "hellhost-d12d4",
+};
+
+// Init
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Providers
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
+// Google Login
+window.googleLogin = function () {
+  signInWithPopup(auth, googleProvider)
+    .then((result) => {
+      document.getElementById("user").innerText =
+        "Welcome " + result.user.displayName;
+    })
+    .catch((error) => alert(error.message));
+};
+
+// GitHub Login
+window.githubLogin = function () {
+  signInWithPopup(auth, githubProvider)
+    .then((result) => {
+      document.getElementById("user").innerText =
+        "Welcome " + result.user.displayName;
+    })
+    .catch((error) => alert(error.message));
+};
